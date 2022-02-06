20cc4a319a1f3cf9a705bc043f2da9ebdcf5c538
Geoffrey Gichaga <geoffrey.githinji@student.moringaschool.com>
2022-02-06 23:11:21 +0300

    ft:Javascript-Functions - Add transition functions on portal side Nav

---
+++ js/portal.js
@@ -0,0 +1,25 @@
+
+function homeFunction()
+{
+    window.location="./index.html";
+}
+
+
+function testsFunction()
+{
+    window.location="./index.html";
+}
+function logoutFunction()
+{
+    alert("You are about to logout of your account")
+    window.location="./index.html";
+}
+
+
+
+function myFunction()
+{
+    window.location="./pages/JSQuiz.html";
+}
+
+
